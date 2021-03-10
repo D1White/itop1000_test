12c4f1f649be88a14c7d9ce218a33ee9a149719f
D1White <danylo.bilyi@outlook.com>
2021-03-10 17:52:15 +0200

    connect to db

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,20 @@
-console.log('HEllosa');
+import dotenv from 'dotenv';
+dotenv.config();
+
+require('./core/db');
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`SERVER RUNNING at http://localhost:${process.env.PORT}`);
+});
